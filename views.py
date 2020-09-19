c257119b59c160b5a7ca8fe9fced5f75509e1261
kahenya-anita <anitakahenya1@gmail.com>
2020-09-20 00:36:12 +0300

    Initialize the app

--- views.py
+++ views.py
@@ -1,13 +1,16 @@
-from flask import Flask,render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect
+from app import app
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/home')
 def home():
     return  render_template('Home.html')
 
-@app.route('/about')
+@app.route('/')
 def about():
     return  render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
